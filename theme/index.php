2ac7553b139e291560a4f19f01129486e82dcab0
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2015-09-06 17:11:14 +0300

    step 3 ed

--- theme/index.php
+++ theme/index.php
@@ -3,7 +3,7 @@
     <main class="col-md-9">
     <?php while ( have_posts() ) : the_post(); ?>
 
-      <article class="post" <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+      <article <?php post_class('post'); ?> id="post-<?php the_ID(); ?>">
         <header>
           <h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
           <span>Posted on <time datetime="2015-06-17 10:25"><?php the_date();?> at <?php the_time();?></time> by <?php the_author();?></span>
